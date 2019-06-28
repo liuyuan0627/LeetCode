155f354cdddffb2fd9526db111155c2a166d1565
liuyuan0627 <liuyuan0627@live.cn>
2019-06-28 11:28:42 +0800

    Update 1_Two_Sum.py

--- python/1_Two_Sum.py
+++ python/1_Two_Sum.py
@@ -5,7 +5,17 @@ class Solution(object):
         :type target: int
         :rtype: List[int]
         """
+        
+        # Solution 1
+        """
         for i in range(len(nums)):
             for j in range(i+1, len(nums)):
                 if nums[i] + nums[j] == target:
                     return [i, j]
+        """
+        
+        # Solution 2
+        for i in range(len(nums)):
+            if target - nums[i] in nums[i+1:]:
+                j = nums[i+1:].index(target - nums[i])
+                return [i, i+j+1]
